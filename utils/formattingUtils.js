da1795664d5b27b7c6242c67117ca20ad7ff5f1a
Johnathon Bowers <johnathonbowers@gmail.com>
2023-11-24 22:45:47 -0500

    Update regex to match entries not beginning with a hyphen
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- utils/formattingUtils.js
+++ utils/formattingUtils.js
@@ -3,8 +3,14 @@ import { PREFIXES } from "../config/constants.js";
 // Function for formatting changelog entry, PR number and PR prLink
 // that returns a list of [formattedChangelogEntry, prefix]
 export const prepareChangesetEntry = (changelogEntry, prNumber, prLink) => {
-  const prefixeRegex = PREFIXES.join("|");
-  const regex = new RegExp(`-\\s(${prefixeRegex}):(.*)`);
+  const prefixRegex = PREFIXES.join("|");
+  
+  /* Updated regex allows for the following scenarios: 
+  1. Lines that do not begin with a hyphen (e.g., "feat: new feature")
+  2. Lines that do not begin with a hyphen and include whitespace (e.g., "  feat: new feature")
+  3. Lines that begin with non-alphanumeric characters besides hyphens (e.g., "#! feat: new feature")
+  */
+  const regex = new RegExp(`[^a-zA-Z0-9]*-?\\s*(${prefixRegex}):(.*)`);
   const match = changelogEntry.match(regex);
   if (match) {
     const [, prefix, text] = match;
