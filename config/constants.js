7d8ba2694c9a111f354427f5d1318b0cee105317
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-25 18:01:13 +0000

    updating constants adding new regex patterns and max size of chanelog entries
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,7 @@
+// ****************************************************
+// I) ENTRY PREFIXES
+// ****************************************************
+
 // Define the prefixes that can be used in the changelog entries
 export const PREFIXES = [
   "breaking",
@@ -8,15 +12,38 @@ export const PREFIXES = [
   "doc",
   "chore",
   "refactor",
+
   "test",
 ];
 
-// Define the regex pattern for changelog entries
+// ****************************************************
+// II) REGXES PATTERNS
+// ****************************************************
+
+// i) Define regex pattern for changelog entries
 export const CHANGELOG_HEADING = "## Changelog";
 export const CHANGELOG_SECTION_REGEX = new RegExp(
   `${CHANGELOG_HEADING}\\s*([\\s\\S]*?)(?:\\n##|$)`
 );
-// Regex explanation:
-// /## Changelog\s*   // Matches the '## Changelog' heading in markdown. '\s*' matches any whitespace (spaces, tabs) following 'Changelog'.
-// ([\s\S]*?)         // Non-greedy match for any character set. Captures the content after '## Changelog' heading.
-// (?:\n##|$)/;       // Non-capturing group. Matches a newline followed by '##' (next markdown heading) or the end of the text ('$').
+/* Regex explanation:
+ /## Changelog\s*   // Matches the '## Changelog' heading in markdown. '\s*' matches any whitespace (spaces, tabs) following 'Changelog'.
+ ([\s\S]*?)         // Non-greedy match for any character set. Captures the content after '## Changelog' heading.
+ (?:\n##|$)/;       // Non-capturing group. Matches a newline followed by '##' (next markdown heading) or the end of the text ('$').
+*/
+
+// ii) Define regex pattern for specific formatting of changelog entries
+export const ENTRY_FORMATTING_PATTERN_REGEX = new RegExp(
+  `[^a-zA-Z0-9]*-?\\s*(${PREFIXES.join("|")}):(.*)`
+);
+/* Regex allows for the following scenarios:
+  1. Lines that do not begin with a hyphen (e.g., "feat: new feature")
+  2. Lines that do not begin with a hyphen and include whitespace (e.g., "  feat: new feature")
+  3. Lines that begin with non-alphanumeric characters besides hyphens (e.g., "#! feat: new feature")
+*/
+
+// ****************************************************
+// III) OTHER CONSTANTS
+// ****************************************************
+
+// Define the maximum length of a changelog entry
+export const MAX_ENTRY_LENGTH = 50;
