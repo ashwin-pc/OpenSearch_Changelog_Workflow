31d610df041d341e8ee717aced96c04cf38c8198
Johnathon Bowers <johnathonbowers@gmail.com>
2023-11-24 21:55:26 -0500

    Update 'core' and 'github' imports with ES Module syntax
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import * as core from '@actions/core';
+import * as github from '@actions/github'
 
 import { extractChangelogEntries } from "./utils/changelogParser.js";
 import { prepareChangesetEntry } from "./utils/formattingUtils.js";
