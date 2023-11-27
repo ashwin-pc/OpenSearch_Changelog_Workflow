beffd74d5d22b9e4067ccbc8bd9b189048704dba
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-27 16:43:43 +0000

    updating regex constants and allowed entry prefixes
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,13 @@
-// ****************************************************
-// I) ENTRY PREFIXES
-// ****************************************************
+/**
+ * ****************************************************
+ * I) ENTRY PREFIXES
+ * ****************************************************
+ */
 
-// Define the prefixes that can be used in the changelog entries
+/**
+ * Define the prefixes that can be used in the changelog entries.
+ * @type {string[]}
+ */
 export const PREFIXES = [
   "breaking",
   "deprecate",
@@ -14,36 +19,55 @@ export const PREFIXES = [
   "refactor",
   "security",
   "test",
+  "skip",
 ];
 
-// ****************************************************
-// II) REGXES PATTERNS
-// ****************************************************
+/**
+ * ****************************************************
+ * II) REGEX PATTERNS
+ * ****************************************************
+ */
 
-// i) Define regex pattern for changelog entries
+/**
+ * Define regex pattern for the changelog heading.
+ * @type {string}
+ */
 export const CHANGELOG_HEADING = "## Changelog";
+
+/**
+ * Regex pattern to capture the content of the changelog section.
+ * @type {RegExp}
+ */
 export const CHANGELOG_SECTION_REGEX = new RegExp(
   `${CHANGELOG_HEADING}\\s*([\\s\\S]*?)(?:\\n##|$)`
 );
-/* Regex explanation:
- /## Changelog\s*   // Matches the '## Changelog' heading in markdown. '\s*' matches any whitespace (spaces, tabs) following 'Changelog'.
- ([\s\S]*?)         // Non-greedy match for any character set. Captures the content after '## Changelog' heading.
- (?:\n##|$)/;       // Non-capturing group. Matches a newline followed by '##' (next markdown heading) or the end of the text ('$').
-*/
+// Explanation:
+// - Matches the '## Changelog' heading in markdown.
+// - '\s*' matches any whitespace following 'Changelog'.
+// - '([\s\S]*?)' is a non-greedy match capturing the content.
+// - '(?:\n##|$)' matches a newline followed by '##' or the end of the text.
 
-// ii) Define regex pattern for specific formatting of changelog entries
+/**
+ * Regex pattern for specific formatting of changelog entries.
+ * @type {RegExp}
+ */
 export const ENTRY_FORMATTING_PATTERN_REGEX = new RegExp(
-  `[^a-zA-Z0-9]*-?\\s*([a-zA-Z0-9]+):(.*)`
+  `-\\s*([a-zA-Z0-9]+):?(.*)?`
 );
-/* Regex allows for the following scenarios:
-  1. Lines that do not begin with a hyphen (e.g., "feat: new feature")
-  2. Lines that do not begin with a hyphen and include whitespace (e.g., "  feat: new feature")
-  3. Lines that begin with non-alphanumeric characters besides hyphens (e.g., "#! feat: new feature")
-*/
+// Explanation:
+// - '-\\s*' matches a hyphen followed by any number of whitespace characters.
+// - '([a-zA-Z0-9]+)' is the first capturing group, matching one or more alphanumeric characters, representing the keyword.
+// - ':?' optionally matches a colon. The '?' makes the colon optional.
+// - '(.*)?' is the second capturing group, matching any characters following the optional colon. The '?' makes this entire group optional.
 
-// ****************************************************
-// III) OTHER CONSTANTS
-// ****************************************************
+/**
+ * ****************************************************
+ * III) OTHER CONSTANTS
+ * ****************************************************
+ */
 
-// Define the maximum length of a changelog entry
+/**
+ * Define the maximum length of a changelog entry.
+ * @type {number}
+ */
 export const MAX_ENTRY_LENGTH = 50;
