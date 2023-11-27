ddfd3360aa5732bc6f2824b1bbad2d2cd01c428f
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-27 16:44:05 +0000

    updating tester code
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- tester/changelogTester.js
+++ tester/changelogTester.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
 import { extractChangelogEntries } from '../utils/changelogParser.js';
+import { prepareChangesetEntryMap } from '../utils/formattingUtils.js';
 
 try {
   const prDescription = fs.readFileSync('tester/sample.txt', 'utf8');
   const data = extractChangelogEntries(prDescription);
-  console.log(data);
+  const data_2 = prepareChangesetEntryMap(data, '1', '');
+  console.log(data_2);
 } catch (err) {
   console.error(err);
 }
