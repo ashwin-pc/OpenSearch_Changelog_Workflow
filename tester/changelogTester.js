062b5e779b71b7b920286f4b13ee798f7bbb232b
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-28 00:42:01 +0000

    updating tester code
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- tester/changelogTester.js
+++ tester/changelogTester.js
@@ -1,12 +1,21 @@
 import fs from 'fs';
 import { extractChangelogEntries } from '../utils/changelogParser.js';
 import { prepareChangesetEntryMap } from '../utils/formattingUtils.js';
-
+import { CategoryWithSkipOptionError } from '../utils/customErrors.js';
 try {
   const prDescription = fs.readFileSync('tester/sample.txt', 'utf8');
   const data = extractChangelogEntries(prDescription);
-  const data_2 = prepareChangesetEntryMap(data, '1', '');
-  console.log(data_2);
+  const entryMap = prepareChangesetEntryMap(data, '1', '');
+  console.log(entryMap);
+
+  if (entryMap["skip"]) {
+    if (Object.keys(entryMap).length > 1) {
+      throw new CategoryWithSkipOptionError();
+    } else {
+      console.log("No changeset file created or updated.");
+    }
+  }
+
 } catch (err) {
   console.error(err);
 }
