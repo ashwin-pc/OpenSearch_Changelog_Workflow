5bacd0689b1db2c0a84a0c8c3a1bc19ed3f99700
Johnathon Bowers <johnathonbowers@gmail.com>
2023-11-30 14:26:48 -0500

    Move 'processLine' fn to avoid reference error
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- utils/changelogParser.js
+++ utils/changelogParser.js
@@ -1,45 +1,8 @@
 import { CHANGELOG_SECTION_REGEX } from "../config/constants.js";
-import { NoChangelogSectionFoundError } from "./customErrors.js";
+import { InvalidChangelogHeadingError } from "./customErrors.js";
 
 // **************************************************************
-// I) EXPORTED FUNCTIONS
-// **************************************************************
-/**
- * Extracts changelog entries from a PR description.
- * @param {string} prDescription - The PR description text in markdown format.
- * @return {string[]} An array of changelog entry strings.
- */
-export const extractChangelogEntries = (prDescription) => {
-  // Match the changelog section using the defined regex
-  const changelogSection = prDescription.match(CHANGELOG_SECTION_REGEX);
-  // Output -> Array of length 2:
-  // changelogSection[0]: Full regex match including '## Changelog' and following content.
-  // changelogSection[1]: Captured content after '## Changelog', excluding the heading itself.
-
-  // Throw error if no changelog section is found
-  if (!changelogSection) {
-    throw new NoChangelogSectionFoundError();
-  }
-  // Initial accumulator for reduce: empty array for lines and initial state
-  const initialAcc = { entries: [], state: { inComment: false } };
-
-  // Process each line and filter out valid changelog entries
-  const changelogEntries = changelogSection[1]
-    .split("\n")
-    .reduce((acc, line) => {
-      const { entries, state } = acc;
-      const processed = processLine(line, state);
-      if (processed.line) entries.push(processed.line);
-      return { entries, state: processed.state };
-    }, initialAcc).entries;
-
-  console.log(`Found ${changelogEntries.length} changelog entries.`);
-
-  return changelogEntries;
-};
-
-// **************************************************************
-// II) INTERNAL FUNCTIONS
+// I) INTERNAL FUNCTIONS
 // **************************************************************
 /**
  * Processes a line of text to determine if it's a valid changelog entry.
@@ -68,4 +31,41 @@ const processLine = (line, state) => {
 
   // For lines within comments or empty lines, return null
   return { state, line: null };
+};
+
+// **************************************************************
+// II) EXPORTED FUNCTIONS
+// **************************************************************
+/**
+ * Extracts changelog entries from a PR description.
+ * @param {string} prDescription - The PR description text in markdown format.
+ * @return {string[]} An array of changelog entry strings.
+ */
+export const extractChangelogEntries = (prDescription) => {
+  // Match the changelog section using the defined regex
+  const changelogSection = prDescription.match(CHANGELOG_SECTION_REGEX);
+  // Output -> Array of length 2:
+  // changelogSection[0]: Full regex match including '## Changelog' and following content.
+  // changelogSection[1]: Captured content after '## Changelog', excluding the heading itself.
+  console.log(changelogSection);
+  // Throw error if '## Changelog' header is missing or malformed
+  if (!changelogSection) {
+    throw new InvalidChangelogHeadingError();
+  }
+  // Initial accumulator for reduce: empty array for lines and initial state
+  const initialAcc = { entries: [], state: { inComment: false } };
+
+  // Process each line and filter out valid changelog entries
+  const changelogEntries = changelogSection[1]
+    .split("\n")
+    .reduce((acc, line) => {
+      const { entries, state } = acc;
+      const processed = processLine(line, state);
+      if (processed.line) entries.push(processed.line);
+      return { entries, state: processed.state };
+    }, initialAcc).entries;
+
+  console.log(`Found ${changelogEntries.length} changelog entries.`);
+
+  return changelogEntries;
 };
