01712aba61bb15bdf989bda24129597a557551cd
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-12-05 16:35:00 +0000

    refactoring code, introducing beforeAll() and nested describe() statements
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- tests/formattingUtils.test.js
+++ tests/formattingUtils.test.js
@@ -1,50 +1,71 @@
-import { 
-    prepareChangesetEntryMap, 
-    prepareChangesetEntry, 
-    prepareChangesetEntriesContent 
+import {
+  prepareChangesetEntryMap,
+  prepareChangesetEntry,
+  prepareChangesetEntriesContent,
 } from "../utils";
 
-describe('prepareChangesetEntryMap', () => {
-    
-    test('should return an object with changeset entries categorized by their prefixes', () => {
+describe("Formatting Utils Tests", () => {
+  let prNumber, prLink, changesetEntries;
 
-        // This array of strings is returned by the extractChangelogEntries function in the workflow, provided the "Changelog" section of the PR description is valid.
-        const changesetEntries = [ '- feat: Adds one feature', '- fix: Fixes a bug', '- feat: Adds a second feature'];
-        const prNumber = 123;
-        const prLink = "https://github.com/TestUser/OpenSearch-Dashboards/pull/123";
-
-        const expectedChangesetEntryMap = {
-            feat: [
-                '- Adds one feature ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))',
-                '- Adds a second feature ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))'
-              ],
-            fix: [
-            '- Fixes a bug ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))'
-            ]
-        };
-
-        const actualChangesetEntryMap = prepareChangesetEntryMap(changesetEntries, prNumber, prLink);
-
-        expect(actualChangesetEntryMap).toEqual(expectedChangesetEntryMap);
-    });
-
-    test()
+  beforeAll(() => {
+    prNumber = 123;
+    prLink = "https://github.com/TestUser/OpenSearch-Dashboards/pull/123";
+    // This array of strings is returned by the extractChangelogEntries function in the workflow, provided the "Changelog" section of the PR description is valid.
+    changesetEntries = [
+      "- feat: Adds one feature",
+      "- fix: Fixes a bug",
+      "- feat: Adds a second feature",
+    ];
 });
 
-describe('prepareChangesetEntry', () => {
+  describe("prepareChangesetEntryMap", () => {
+    test("should return an object with changeset entries categorized by their prefixes", () => {
 
-    test('when provided with a valid changelog entry, should return a tuple containing the formatted changelog entry and the identified prefix', () => {
+      const expectedChangesetEntryMap = {
+        feat: [
+          "- Adds one feature ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))",
+          "- Adds a second feature ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))",
+        ],
+        fix: [
+          "- Fixes a bug ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))",
+        ],
+      };
 
-        const changelogEntry = "- feat: Adds one feature";
-        const prNumber = 123;
-        const prLink = "https://github.com/TestUser/OpenSearch-Dashboards/pull/123";
+      const actualChangesetEntryMap = prepareChangesetEntryMap(
+        changesetEntries,
+        prNumber,
+        prLink
+      );
 
-        const expectedTuple = ['- Adds one feature ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))', 'feat'];
-
-        const actualTuple = prepareChangesetEntry(changelogEntry, prNumber, prLink);
-
-        expect(actualTuple).toEqual(expectedTuple);
-
+      expect(actualChangesetEntryMap).toEqual(expectedChangesetEntryMap);
     });
+  });
 
+  describe("prepareChangesetEntry", () => {
+    test("when provided with a single valid changeset entry, should return a tuple containing the formatted changeset entry and the identified prefix", () => {
+
+      const expectedTupleOne = [
+        "- Adds one feature ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))",
+        "feat",
+      ];
+      const expectedTupleTwo = [
+        "- Fixes a bug ([#123](https://github.com/TestUser/OpenSearch-Dashboards/pull/123))",
+        "fix",
+      ];
+
+      const actualTupleOne = prepareChangesetEntry(
+        changesetEntries[0],
+        prNumber,
+        prLink
+      );
+      const actualTupleTwo = prepareChangesetEntry(
+        changesetEntries[1],
+        prNumber,
+        prLink
+      );
+
+      expect(actualTupleOne).toEqual(expectedTupleOne);
+      expect(actualTupleTwo).toEqual(expectedTupleTwo);
+    });
+  });
 });
