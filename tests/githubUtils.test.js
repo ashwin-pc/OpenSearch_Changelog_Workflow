493d33a315ad2c0f0e9d964f70d3640c03c4370b
Johnathon Bowers <johnathonbowers@gmail.com>
2023-12-09 21:47:08 -0500

    Add unit tests for postPRComment
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- tests/githubUtils.test.js
+++ tests/githubUtils.test.js
@@ -5,8 +5,16 @@ import {
   createOrUpdateFile,
   PullRequestDataExtractionError,
   ChangesetFileAccessError,
+  InvalidChangelogHeadingError,
+  EmptyChangelogSectionError,
+  EntryTooLongError,
+  InvalidPrefixError,
+  CategoryWithSkipOptionError,
+  ChangelogEntryMissingHyphenError,
+  EmptyEntryDescriptionError,
   postPRComment,
 } from "../utils";
+import { GITHUB_TOKEN } from "../config/constants";
 
 // Mock the @actions/github module
 jest.mock("@actions/github", () => ({
@@ -191,6 +199,152 @@ describe("Github Utils Tests", () => {
         name: label,
       });
       expect(mockRemoveLabel).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("postPRComment", () => {
+    // Mock the createComment method
+    const mockCreateComment = jest.fn();
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+      github.getOctokit.mockImplementation(() => ({
+        rest: {
+          issues: {
+            createComment: mockCreateComment,
+          }
+        }
+      }));
+    });
+
+    test("calls getOctokit with the correct token", async () => {
+      const error = new PullRequestDataExtractionError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(github.getOctokit).toHaveBeenCalledWith(GITHUB_TOKEN);
+    });
+
+    test("does not generate a comment for a PullRequestDataExtractionError", async () => {
+      const error = new PullRequestDataExtractionError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).not.toHaveBeenCalled();
+    });
+
+    test("does not generate a comment for a ChangesetFileAccessError", async () => {
+      const error = new ChangesetFileAccessError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).not.toHaveBeenCalled();
+    });
+
+    test("generates the correct comment for InvalidChangelogHeadingError", async () => {
+      const error = new InvalidChangelogHeadingError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).toHaveBeenCalledWith({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: `Invalid Changelog Heading Error: ${error.message}`
+      });
+    });
+
+    test("generates the correct comment for EmptyChangelogSectionError", async () => {
+      const error = new EmptyChangelogSectionError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).toHaveBeenCalledWith({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: `Empty Changelog Section Error: ${error.message}`
+      });
+    });
+
+    test("generates the correct comment for EntryTooLongError", async () => {
+      const error = new EntryTooLongError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).toHaveBeenCalledWith({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: `Entry Too Long Error: ${error.message}`
+      });
+    });
+
+    test("generates the correct comment for InvalidPrefixError", async () => {
+      const error = new InvalidPrefixError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).toHaveBeenCalledWith({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: `Invalid Prefix Error: ${error.message}`
+      });
+    });
+
+    test("generates the correct comment for CategoryWithSkipOptionError", async () => {
+      const error = new CategoryWithSkipOptionError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).toHaveBeenCalledWith({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: `Category With Skip Option Error: ${error.message}`
+      });
+    });
+
+    test("generates the correct comment for ChangelogEntryMissingHyphenError", async () => {
+      const error = new ChangelogEntryMissingHyphenError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).toHaveBeenCalledWith({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: `Changelog Entry Missing Hyphen Error: ${error.message}`
+      });
+    });
+    
+    test("generates the correct comment for EmptyEntryDescriptionError", async () => {
+      const error = new EmptyEntryDescriptionError();
+      await postPRComment(owner, repo, prNumber, error);
+      expect(mockCreateComment).toHaveBeenCalledWith({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: `Empty Entry Description Error: ${error.message}`
+      });
+    });
+
+    test("logs success message after posting comment to PR", async () => {
+      const error = new InvalidChangelogHeadingError();
+      jest.spyOn(console, "log");
+      await postPRComment(owner, repo, prNumber, error);
+      expect(console.log).toHaveBeenCalledWith(`Comment posted to PR #${prNumber}: "Invalid Changelog Heading Error: ${error.message}"`);
+    });
+
+    test("logs error message if posting comment to PR fails", async () => {
+      const error = new InvalidChangelogHeadingError();
+      jest.spyOn(console, "error");
+      mockCreateComment.mockRejectedValueOnce(apiError);
+      await postPRComment(owner, repo, prNumber, error);
+      expect(console.error).toHaveBeenCalledWith(`Error posting comment to PR #${prNumber}: ${apiError.message}`);
+    });
+
+    test("logs messsage if no comment is posted to the PR because of the error type", async () => {
+      const error = new PullRequestDataExtractionError();
+      jest.spyOn(console, "log");
+      await postPRComment(owner, repo, prNumber, error);
+      expect(console.log).toHaveBeenCalledWith(`No comment posted to PR #${prNumber} due to error type: ${error.constructor.name}`);
+    });
+
+    test("handles unexpected errors gracefully", async () => {
+      const error = new InvalidChangelogHeadingError();
+      const unexpectedError = new Error("Unexpected error");
+
+      // Override the mock implementation for this test
+      mockCreateComment.mockImplementationOnce(() => {
+        throw unexpectedError;
+      });
+      jest.spyOn(console, "error");
+      await postPRComment(owner, repo, prNumber, error);
+      expect(console.error).toHaveBeenCalledWith(`Error posting comment to PR #${prNumber}: ${unexpectedError.message}`)
     });
   });
 
