6d7a1129076135c8187f62941ebdb08a3ff509d8
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-02-22 14:52:45 -0300

    updating src/services/index.js
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src/services/index.js
+++ src/services/index.js
@@ -2,4 +2,5 @@ export * from './comment.services.js';
 export * from './file.services.js';
 export * from './file.forked.services.js';
 export * from './auth.services.js';
+export * from './auth.forked.services.js';
 export * from './label.services.js';
