381196a7d3bc24e5a1fae5eba80193e38d5e6d26
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-03-15 17:34:30 -0300

    refactoring main
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src/utils/validators.utils.js
+++ src/utils/validators.utils.js
@@ -92,7 +92,10 @@ export const isGitHubAppInstalledOrNotSuspended = async (prData) => {
       prData.headOwner,
       prData.headRepo
     );
-
+  console.log(data)
+  console.log(githubAppInstallationInfo)
+  console.log(githubAppInstallationInfo.installed)
+  console.log(githubAppInstallationInfo.suspended)
   if (
     githubAppInstallationInfo.installed ||
     !githubAppInstallationInfo.suspended
