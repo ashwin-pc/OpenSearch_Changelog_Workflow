f498d8771e0688fc92bca60669cffcb630b67de8
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-03-21 11:38:06 -0300

    updating warning message for github app not installed or suspended to an info message
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src/infos/prBridgeService.infos.js
+++ src/infos/prBridgeService.infos.js
@@ -16,7 +16,7 @@ export class GitHubAppSuspendedOrNotInstalledInfo {
     const message =
       `Please ensure **manual commit for changeset file _${prNumber}.yml_** under folder _changelogs/fragments_ to complete this PR` +
       `\n\n` +
-      `If you want to use the available GitHub App [GITHUB_APP_NAME](${GITHUB_APP_NAME})to avoid manual creation of changeset file you can install it in your forked repository following this [link](${GITHUB_APP_INSTALLATION_LINK}). ` +
+      `If you want to use the available ${GITHUB_APP_NAME} to avoid manual creation of changeset file you can install it in your forked repository following this [link](${GITHUB_APP_INSTALLATION_LINK}). ` +
       `\n\n` +
       `For more information about formatting of changeset files, please visit [${AUTO_CHANGESET_AND_RELEASE_NOTES_TOOL_NAME}](${AUTO_CHANGESET_AND_RELEASE_NOTES_TOOL_DOCS}).`;
     this.name = this.constructor.name;
