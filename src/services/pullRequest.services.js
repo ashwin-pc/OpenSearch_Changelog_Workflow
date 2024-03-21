0f3b91b5cd6409dfa35fe4640737384cf9f7129f
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-03-21 15:58:36 -0300

    updating pullRequests.services.js
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src/services/pullRequest.services.js
+++ src/services/pullRequest.services.js
@@ -12,7 +12,7 @@
 const getFileInCommitedChanges = async (octokit, owner, repo, prNumber, path) => {
   try {
     // Get the list of files for the pull request
-    const { data: files } = await octokit.pulls.listFiles({
+    const { data: files } = await octokit.rest.pulls.listFiles({
       owner,
       repo,
       prNumber,
