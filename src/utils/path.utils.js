f9f0977ce3cf46fe44de3cf94722d8998069a50c
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-04-02 11:01:45 -0300

    updating location for CHANGESET_PATH const in path.utils.js
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src/utils/path.utils.js
+++ src/utils/path.utils.js
@@ -1,4 +1,4 @@
-import { CHANGESET_PATH } from '../config/constants.index.js';
+import { CHANGESET_PATH } from '../config/constants.js';
 
 export const getChangesetFilePath = (prNumber) => {
   return `${CHANGESET_PATH}/${prNumber}.yml`;
