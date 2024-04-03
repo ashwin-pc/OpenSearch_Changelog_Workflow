e8af509bd0c1cbd57774157eb0aff17f838880b6
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-04-03 18:10:46 -0300

    updating regex for ENTRY_FORMATTING_PATTERN_REGEX
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src/config/constants.js
+++ src/config/constants.js
@@ -72,7 +72,7 @@ export const CHANGELOG_SECTION_REGEX = new RegExp(
  * @type {RegExp}
  */
 export const ENTRY_FORMATTING_PATTERN_REGEX = new RegExp(
-  `([^a-zA-Z])?\\s*([a-zA-Z0-9]+):?(.*)?`
+  `([^a-zA-Z])?\\s*(.*):?(.*)?`
 );
 // Explanation:
 // - '-\\s*' matches a hyphen followed by any number of whitespace characters up until the first capturing group.
